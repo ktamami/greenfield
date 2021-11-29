b04f630b73f9511616ec7925690a6810cbe94f7a
Tamami <85936708+ktamami@users.noreply.github.com>
2021-11-29 16:29:54 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -103,4 +103,4 @@ def failed():
     return render_template("failed.html", current_year=current_year)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
